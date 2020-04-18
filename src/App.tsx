334b4a5811cb8afc7fcd7af8ac9bedd201b081e1
Jonathan Krone <jonathankrone@gmail.com>
2020-04-18 09:23:43 -0500

    document some initial thoughts on the app and possible component structure/data concerns

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TripExplorer">
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+/**
+ * Initial App structure thoughts
+ *
+ * container
+ *  left sidebar Route and Route-Direction selector
+ *    fetch available routes
+ *    display all Routes with North/South buttons aside them
+ *    when a route is selected, update the URL
+ *  Right content area for Route-Stops
+ *    when no route is selected
+ *      display some cute waiting signal?
+ *    when a route is selected
+ *      fetch list of directions
+ *      render that list
+ *      Optional: add a link to a Google Map location for each stop
+ *      Stretch: Render stops layed over a Google/Mapbox map widget
+ *
+ * To enable back/forward, we probably want to have Route and Route-Direction be a URL-parameter
+ *
+ * Stretch thought: We would probably get into NextTrip rate limiting if we were to pull all
+ * Route-Stops for all Routes, so that we could overlay them all on top of a Map at once. But
+ * only in a real high-volume (production) usage, so maybe we can get away with it for this
+ * test app :)
+ */
