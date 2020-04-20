1b6e80dff25d7fa6eecbd74a11146767bf3a7058
Jonathan Krone <jonathankrone@gmail.com>
2020-04-20 12:15:25 -0500

    add a simple test for StopItem

---
+++ src/components/StopItem.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import StopItem from './StopItem'
+
+const testStop = { Text: 'P. Sherman 42 Wallaby Way, Sydney', Value: '1' }
+
+describe('StopItem', () => {
+  it('renders', () => {
+    const { asFragment, getByText } = render(<StopItem item={testStop} />)
+
+    expect(getByText(/Wallaby/)).toBeTruthy()
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
